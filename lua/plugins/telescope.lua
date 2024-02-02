afbe3c68c3a85e1cc514c235809d955d8052f878
dbubel <50341559+dbubel@users.noreply.github.com>
2024-02-02 13:45:50 -0500

    lock in package version via lock.json

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,8 +9,9 @@ return {
       -- show git commits
       vim.keymap.set("n", "<leader>gc", "<cmd> Telescope git_commits <CR>")
 
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
       vim.keymap.set("n", "<leader>fj", builtin.current_buffer_fuzzy_find, {})
