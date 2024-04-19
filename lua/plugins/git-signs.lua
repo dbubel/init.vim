f47de9cfc49140e9e1d428910af6d7247a1a5a66
dbubel <50341559+dbubel@users.noreply.github.com>
2024-04-19 11:22:14 -0400

    updated mason

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -3,11 +3,10 @@ return {
 	config = function()
 		local gitsigns = require("gitsigns")
 		gitsigns.setup({})
-    vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk <CR>", {})
-    vim.keymap.set("n", "<leader>hD", ":Gitsigns diffthis <CR>", {})
-    vim.keymap.set("n", "<leader>hf", ":Gitsigns next_hunk <CR>", {})
-    vim.keymap.set("n", "<leader>hb", ":Gitsigns prev_hunk <CR>", {})
-    vim.keymap.set("n", "<leader>hp", ":Gitsigns preview_hunk <CR>", {})
+		vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk <CR>", {})
+		vim.keymap.set("n", "<leader>hD", ":Gitsigns diffthis <CR>", {})
+		vim.keymap.set("n", "<leader>hf", ":Gitsigns next_hunk <CR>", {})
+		vim.keymap.set("n", "<leader>hb", ":Gitsigns prev_hunk <CR>", {})
+		vim.keymap.set("n", "<leader>hp", ":Gitsigns preview_hunk <CR>", {})
 	end,
 }
-
