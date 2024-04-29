b1ea3b7b979459d92caa8ef08f508cbb4127bf86
dbubel <50341559+dbubel@users.noreply.github.com>
2024-04-29 14:50:58 -0400

    tweak

--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,13 +1,13 @@
- return {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-            require('rose-pine').setup({
-                disable_background = true
-            })
-            vim.cmd.colorscheme('rose-pine')
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        end
-    }
+return {
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    -- vim.cmd("colorscheme rose-pine")
+    require("rose-pine").setup({
+      disable_background = true,
+    })
+    vim.cmd.colorscheme("rose-pine")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end,
+}
