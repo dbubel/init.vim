410f30aec7ce3e7ec0aa4cf1d6a36f21987b2b86
dbubel <50341559+dbubel@users.noreply.github.com>
2024-05-06 11:16:09 -0400

    add go-ray plugin for running tests

--- lua/key-binds.lua
+++ lua/key-binds.lua
@@ -5,11 +5,13 @@ vim.keymap.set("n", "<leader>Y", '"+Y')
 vim.keymap.set("n", "<C-s>", "<cmd> w <CR>")
 vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<Esc>", "<cmd> noh <CR>")
+
 -- move windows up down left right
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
+
 -- line numbers normal and relative
 vim.keymap.set("n", "<leader>n", "<cmd> set nu! <CR>")
 vim.keymap.set("n", "<leader>rn", "<cmd> set rnu! <CR>")
